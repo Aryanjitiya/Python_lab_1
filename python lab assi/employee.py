fec707f599b571528e2704655e88e2ab174760ab
Aryanjitiya <bca2022aryan1730@tnraocollege.org>
2024-07-15 21:42:23 +0530

    Add files via upload

---
+++ python lab assi/employee.py
@@ -0,0 +1,13 @@
+EMPNAME = []
+
+EMPNAME. append("Aaryan")
+EMPNAME. append("Harsh")
+EMPNAME.append("Manav")
+
+print("Employee Names after additions:", EMPNAME)
+EMPNAME.remove("Harsh")
+
+print("Employee Names after removal  :", EMPNAME)
+EMPNAME. append("Maulik")
+
+print("Final Employee Names          :", EMPNAME)
